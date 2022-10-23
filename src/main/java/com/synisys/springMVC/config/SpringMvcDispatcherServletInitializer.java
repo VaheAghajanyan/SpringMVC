5e8bdf8b7877b6ca347ca0d8992be9c0ca93fd64
Vahe <vah.aghajanyan@gmail.com>
2022-10-23 22:46:28 +0400

    2. Configuration with Java code.

---
+++ src/main/java/com/synisys/springMVC/config/SpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,21 @@
+package com.synisys.springMVC.config;
+
+/**
+ * This class is equal to web.xml file
+ */
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
